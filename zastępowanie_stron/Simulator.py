4f82e22dc05592b83181efd615e739b0d2a73605
Krzysztof Jakubiec <253001@student.pwr.edu.pl>
2021-01-04 19:52:43 +0100

    04.01.2021

--- zastępowanie_stron/Simulator.py
+++ zastępowanie_stron/Simulator.py
@@ -9,26 +9,32 @@ class Simulator:
         swaps = 0
         not_swaps = 0
         free_frame = 0
+        counters = []
 
         print(" >Symulacja FIFO<")
         while True:
             if time == len(self.reference_list):
                 self.frames_list.clear()
-                return time, swaps, not_swaps
+                return time, swaps, not_swaps, counters
 
             if len(self.frames_list) != self.frames_size:
                 self.frames_list.append(time)
+                counters.append(0)
 
             if (self.reference_list[time] in self.frames_list) is False:
                 self.frames_list[free_frame] = self.reference_list[time]
                 swaps += 1
+                counters[free_frame] += 1
                 free_frame += 1
                 free_frame %= self.frames_size
             else:
-                not_swaps += 1
+                for i in range(len(self.frames_list)):
+                    if self.reference_list[time] == self.frames_list[i]:
+                        counters[i] += 1
+                        not_swaps += 1
 
+            print(self.frames_list, counters)
             time += 1
-            print(self.frames_list)
 
     def lru_simulation(self):
         time = 0
@@ -40,7 +46,7 @@ class Simulator:
         while True:
             if time == len(self.reference_list):
                 self.frames_list.clear()
-                return time, swaps, not_swaps
+                return time, swaps, not_swaps, counters
             if len(self.frames_list) != self.frames_size:
                 self.frames_list.append(time)
                 counters.append(0)
--- zastępowanie_stron/main.py
+++ zastępowanie_stron/main.py
@@ -4,12 +4,6 @@ import Simulator
 
 menu = TextMenu.Menu()
 generator = Generator.Generator()
-# generator.load_reference_list()
-# generator.generate_reference_list()
-# generator.show_reference_list()
-# simulator = Simulator.Simulator(generator.reference_list)
-# simulator.lru_simulation()
-
 
 while True:
     menu.display()
@@ -27,14 +21,14 @@ while True:
         generator.save_reference_list()
     elif select == 4:
         simulator = Simulator.Simulator(generator.reference_list)
-        sim_time, times_swapped, times_omitted = simulator.fifo_simulation()
+        sim_time, times_swapped, times_omitted, counters = simulator.fifo_simulation()
         print("\nCzas trwania symulacji:", sim_time, "\nLiczba podmian stron:", times_swapped,
-              "\nIle razy odwołanie w ramce:", times_omitted)
+              "\nIle razy odwołanie w ramce:", times_omitted, "\nLiczniki operacji na ramkach:", counters)
     elif select == 5:
         simulator = Simulator.Simulator(generator.reference_list)
-        sim_time, times_swapped, times_omitted = simulator.lru_simulation()
+        sim_time, times_swapped, times_omitted, counters = simulator.lru_simulation()
         print("\nCzas trwania symulacji:", sim_time, "\nLiczba podmian stron:", times_swapped,
-              "\nIle razy odwołanie w ramce:", times_omitted)
+              "\nIle razy odwołanie w ramce:", times_omitted, "\nLiczniki operacji na ramkach:", counters)
     elif select == 6:
         generator.show_reference_list()
     elif select == 7:
